548a3429a0cbe3af21df72de30d19789a033c580
Shivani <shivani@Shivanis-MacBook-Pro.local>
2020-02-10 17:21:21 -0500

    added a infospot to imae

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-const container = document.querySelector('#container')
+const container = document.getElementById('container')
 const panorama = new PANOLENS.ImagePanorama('../assets/images/49479880741_806baa3828_o.jpg');
 const viewer = new PANOLENS.Viewer({container:container});
 viewer.add(panorama);
+
+// add infospot 
+const infospot = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
+infospot.position.set(0, -2000, -5000);
+infospot.addHoverText('SHIVANI U ROCK',40);
+
+panorama.add(infospot);
+
